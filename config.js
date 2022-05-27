4c924e160de75c2d8f5b25d4702566ae98c01805
Arullzzz <106241765+Arullzzz@users.noreply.github.com>
2022-05-28 03:44:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ global.wait = '「 ⏱️ 」Harap tunggu sebentar...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© ArullBotz' //change the watermark 
-global.image = 'https://telegra.ph/file/1a3eac3701775793618b3.jpg' //change the image
+global.image = 'https://telegra.ph/file/Screenshot_20220408-185157~2.png' //change the image
 global.thumbfoto = 'https://telegra.ph/file/ae6f7f701a5be2951648f.jpg'
 
 global.multiplier = 9999999 // Semakin tinggi, semakin sulit naik level
